6ed6b1502d9739bf3e8ff7c56fd00b58ebe11803
jinyell <the.jinyoung.ko@gmail.com>
2019-03-29 14:49:52 -0700

    Unity 2018.3.9f1, Added Best HTTP (Pro)

---
+++ Youtube VR/Assets/_Scripts/Tools/CanvasGroupSetter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tools
+{
+    public static class CanvasGroupSetter
+    {
+        private const float SHOW_ALPHA = 1f;
+        private const float HIDE_ALPHA = 0f;
+
+        public static void Show(CanvasGroup canvasGroup)
+        {
+            Set(canvasGroup, false);
+        }
+
+        public static void Hide(CanvasGroup canvasGroup)
+        {
+            Set(canvasGroup, false);
+        }
+
+        private static void Set(CanvasGroup canvasGroup, bool show)
+        {
+            canvasGroup.alpha = (show == true) ? SHOW_ALPHA : HIDE_ALPHA;
+            canvasGroup.interactable = show;
+            canvasGroup.blocksRaycasts = show;
+            canvasGroup.ignoreParentGroups = show;
+        }
+    }
+}
