d55e5168eda75d146acb1e086e35fe525072cde0
jinyell <the.jinyoung.ko@gmail.com>
2019-04-05 16:03:20 -0700

    Added Oculus Plugin

--- Youtube VR/Assets/_Scripts/SessionAdministrator.cs
+++ Youtube VR/Assets/_Scripts/SessionAdministrator.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BestHTTP;
+using System;
+using BestHTTP.Authentication;
 
 public class SessionAdministrator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const string SEARCH_REQUEST = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=YouTube+Data+API&type=video&key={0}";
+
+    [SerializeField] private string apiKey = string.Empty;
+
+    private void Authenticate()
     {
-        
+        string searchURL = string.Format(SEARCH_REQUEST, apiKey);
+        var request = new HTTPRequest(new Uri(searchURL), (req, resp) =>
+        {
+            if (resp.StatusCode != 401)
+                Debug.Log("Authenticated");
+            else
+                Debug.Log("NOT Authenticated");
+
+
+            if(resp.IsSuccess)
+            {
+                Debug.Log("Successful");
+            }
+            else
+            {
+                Debug.Log("Failure");
+            }
+            Debug.Log(resp.DataAsText);
+        });
+        request.Send();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Authenticate();
     }
 }
